f35ac50354928cb6af35380b719b3ae792a0d420
183aryan <aryan183choudhary@gmail.com>
2024-01-09 19:10:25 +0530

    second commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Button from './Button';
+import LogoutBtn from './Header/LogoutBtn';
+import Container from './container/Container';
+import Logo from './Logo';
+import RTE from './RTE';
+import Login from './Login';
+import Signup from './Signup';
+import PostForm from './post-form/PostForm';
+import PostCard from './PostCard';
+import AuthLayout from './AuthLayout';
+import Input from './Input';
+import Select from './Select';
+
+export {
+    Header,
+    Footer,
+    Button,
+    LogoutBtn,
+    Container,
+    Logo,
+    RTE,
+    Login,
+    Signup,
+    PostForm, 
+    PostCard,
+    AuthLayout,
+    Input,
+    Select,
+}
